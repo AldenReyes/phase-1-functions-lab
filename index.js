b91c631654926be0e4f5103b750e29528ca9e58f
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-03-18 00:32:31 -0700

    Completed Lab; calculated solutions using functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(passengerBlocks) {
+  return Math.abs(passengerBlocks - 42);
+}
+
+function distanceFromHqInFeet(passengerBlocks) {
+//1 block = 264 ft
+  return distanceFromHqInBlocks(passengerBlocks) * 264;
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+  return (Math.abs(endingBlock - startingBlock) * 264);
+}
+
+function calculatesFarePrice(startingBlock, endingBlock) {
+  const fareDistance = (distanceTravelledInFeet(startingBlock, endingBlock));
+  if (fareDistance > 2500) return ('cannot travel that far');
+  if (fareDistance > 2000) return 25;
+  if (fareDistance <= 2000 && fareDistance > 400) return ((fareDistance - 400) * 0.02);
+  else return 0
+}
